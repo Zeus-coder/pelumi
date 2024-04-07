8366c9ebd491b7882a5746d8b8288b75f317409a
OLuwapelumi <freshtunz58@gmail.com>
2024-04-07 09:46:31 +0100

    feature:Navbar,hero,about,work,experience,feedbacks,Tech Stack

---
+++ components/About.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import React from "react";
+import { Tilt } from "react-tilt";
+
+import { motion } from "framer-motion";
+import { styles } from "@/app/style";
+import { services } from "../constants";
+import { SectionWrapper } from "@/hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+import Image from "next/image";
+
+
+
+const ServiceCard = ({ index, title, icon }: { index: number; title: string; icon: string })  => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+      // @ts-ignore
+        options={{
+          
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        // @ts-ignore
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <Image
+          src={services[index].icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant(1)}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I&apos;m a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Nextjs, Firebase, Node.js, and
+        Three.js. I&apos;m a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let&apos;s work together to bring your ideas to life!
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} icon=""/>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
