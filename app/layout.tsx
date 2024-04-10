bbfeea542d4b9a11eed817feca6ae6fda453dd2a
OLuwapelumi <freshtunz58@gmail.com>
2024-04-10 08:37:33 +0100

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {Navbar} from "@/components/index"
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+       <GoogleTagManager gtmId="G-F9S879P7VG" />
       <Navbar/>
       <body className={inter.className}>{children}</body>
     </html>
