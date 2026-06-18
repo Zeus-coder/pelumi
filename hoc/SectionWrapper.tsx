e21649f782e69cf628cf63720c9645d183739ab8
Oluwapelumi <freshtunz58@gmail.com>
2026-06-18 08:16:14 +0100

    Update SectionWrapper.tsx

--- hoc/SectionWrapper.tsx
+++ hoc/SectionWrapper.tsx
@@ -10,7 +10,7 @@ const StarWrapper = (Component: any, idName: string) =>
         variants={staggerContainer( 0.2, 0.5)}
         initial='hidden'
         whileInView='show'
-        viewport={{ once: true, amount: 0.25 }}
+        viewport={{ once: true, amount: 0.1 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
         <span className='hash-span' id={idName}>
